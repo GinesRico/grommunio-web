4ed69bdf6c0cd979c25a74b246197bd2b4ba63d8
GinesRico <ginesrico89@gmail.com>
2026-09-07 11:56:33 +0200

    Disable legacy title counter before startup

--- server/includes/templates/webclient.php
+++ server/includes/templates/webclient.php
@@ -151,6 +151,14 @@ if (!empty($_SESSION['url_action'])) {
 			prefetchedHierarchy	= <?php echo json_encode($prefetchedHierarchy); ?>;
 <?php } ?>
 
+			// The title counter is cosmetic and its legacy bundle can run before
+			// settings/folder models are ready. Disable it for operator sessions
+			// before the webclient startup callback is registered.
+			if (user && (user.operator_mode === true || user.hide_personal_store === true || user.shared_only === true) &&
+				Zarafa.hierarchy && Zarafa.hierarchy.Actions) {
+				Zarafa.hierarchy.Actions.setTitleCounter = function() {};
+			}
+
 			Ext.onReady(Zarafa.loadWebclient, Zarafa);
 		</script>
 	</body>
